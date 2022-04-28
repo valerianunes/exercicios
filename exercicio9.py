f89af3f8c68b23c19a083fc4e81eaacdbe5e6496
Valéria Cabral Nunes <100439216+valerianunes@users.noreply.github.com>
2022-04-28 18:47:03 -0400

    Add files via upload

---
+++ exercicio9.py
@@ -0,0 +1,9 @@
+# Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles.
+num1=int(input("digite um numero--> "))
+num2=int(input("digite outro numero--> "))
+while num2<num1:
+	num1=int(input("digite um numero--> "))
+	num2=int(input("digite outro numero--> "))
+else:
+	for i in range(num1,num2,1):
+		print(i)
