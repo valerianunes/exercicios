4a97b2fea9bd33fae30ff895870a8e61dfe5c362
Valéria Cabral Nunes <100439216+valerianunes@users.noreply.github.com>
2022-04-28 18:22:16 -0400

    Add files via upload

---
+++ exercicio4.py
@@ -0,0 +1,14 @@
+# Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operaçãopopulaçãoA=float(input("informe a população da cidade A "))
+populaçãoA=float(input("informe a população da cidade A "))
+populaçãoB=float(input("informe a população da cidade B "))
+ano=0
+taxa_crescimentoA=float(input("informe a taxa de crescimento da população da cidade A "))
+taxa_crescimentoB=float(input("informe a taxa de crescimento da população da cidade B "))
+while populaçãoA < populaçãoB:
+	populaçãoA+=round((populaçãoA*taxa_crescimentoA)/100)
+	populaçãoB+=round((populaçãoB*taxa_crescimentoB)/100)
+	ano=ano+1
+
+print("levará",ano,"anos para população da cidade A ser maior que a cidade B")
+print("populaçãoB-->",populaçãoB,"habitantes")
+print("populaçãoA-->", populaçãoA,"habitantes")
